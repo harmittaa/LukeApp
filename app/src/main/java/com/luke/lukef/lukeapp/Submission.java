79bacb84c9b29275930e008bee49b8b4caac6321
titay2 <tehetenamas@gmail.com>
2016-11-01 15:45:07 +0200

    skeleton code for submission class with out the test code

---
+++ app/src/main/java/com/luke/lukef/lukeapp/Submission.java
@@ -0,0 +1,36 @@
+package com.luke.lukef.lukeapp;
+
+/**
+ * Created by tehetenamasresha on 01/11/2016.
+ */
+
+import android.graphics.Bitmap;
+
+/**
+ * handles submissions made by users
+ */
+public class Submission {
+    Bitmap image;
+    int location;
+    String title;
+    String catagory;
+
+        public  void add() {
+        }
+
+        public  void edit() {
+        }
+
+        public  void update() {
+        }
+
+        public  void delete() {
+        }
+
+        public  void share() {
+        }
+
+        public  void flag() {
+        }
+}
+
