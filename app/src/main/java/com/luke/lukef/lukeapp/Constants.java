0916fb2097757ab783669b755cd50ab8c8d51887
Daniel <daniel.zakharin>
2016-11-07 13:37:49 +0200

    added bottom bar

--- app/src/main/java/com/luke/lukef/lukeapp/Constants.java
+++ app/src/main/java/com/luke/lukef/lukeapp/Constants.java
@@ -4,4 +4,8 @@ public class Constants {
     public enum fragmentTypes{
         FRAGMENT_CONFIRMATION,FRAGMENT_LEADERBOARD,FRAGMENT_MAP, FRAGMENT_NEW_SUBMISSION,FRAGMENT_POINT_OF_INTEREST,FRAGMENT_PROFILE
     }
+
+    public enum bottomActionBarStates{
+        MAP_CAMERA, BACK_ONLY, BACK_TICK, BACK_REPORT
+    }
 }
