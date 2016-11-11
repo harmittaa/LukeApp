0c400e510a1fe0df84ed5b3360e22e69e57bf46a
Bang Nguyen <YOUR EMAIL ADDRESS>
2016-11-11 20:49:06 +0200

    Add Notification switch and custom header for Drawer

--- app/src/main/java/com/luke/lukef/lukeapp/MainActivity.java
+++ app/src/main/java/com/luke/lukef/lukeapp/MainActivity.java
@@ -1,9 +1,18 @@
 package com.luke.lukef.lukeapp;
 
+import android.animation.ObjectAnimator;
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.support.design.widget.NavigationView;
+
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 
@@ -12,7 +21,9 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
 
 import com.luke.lukef.lukeapp.fragments.ConfirmationFragment;
 import com.luke.lukef.lukeapp.fragments.LeaderboardFragment;
@@ -21,20 +32,54 @@ import com.luke.lukef.lukeapp.fragments.NewSubmissionFragment;
 import com.luke.lukef.lukeapp.fragments.PointOfInterestFragment;
 import com.luke.lukef.lukeapp.fragments.ProfileFragment;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+import static android.R.id.progress;
+
+public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private ImageButton leftButton;
     private ImageButton rightButton;
     private ImageButton midButton;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private int progressStatus;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        final NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        // Setup navigation drawer view
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        setupDrawerContent(navigationView);
+        Menu menu = navigationView.getMenu();
+        //Notification switch handler
+        MenuItem menuItem = menu.findItem(R.id.notification);
+        View actionView = MenuItemCompat.getActionView(menuItem);
+        actionView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            //Do the function here
+            }
+        });
+
+        //Custom header in Navigation Drawer
+        View header = navigationView.getHeaderView(0);
+        progressStatus = 25;
+        Drawable drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.progress_bar, null);
+        progressBar = (ProgressBar)header.findViewById(R.id.progressbar1);
+        // Main Progress
+        progressBar.setProgress(progressStatus);
+        // Maximum Progress
+        progressBar.setMax(100);
+        progressBar.setProgressDrawable(drawable);
+
+        //Animation when drawing process
+        ObjectAnimator animation = ObjectAnimator.ofInt(progressBar, "progress", 0, progressStatus);
+        animation.setDuration(990);
+        animation.setInterpolator(new DecelerateInterpolator());
+        animation.start();
 
         leftButton = (ImageButton) findViewById(R.id.button_left);
         rightButton = (ImageButton) findViewById(R.id.button_right);
@@ -43,7 +88,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setBottomBarButtonsListeners();
         leftButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                drawer.openDrawer(navigationView);
+                drawerLayout.openDrawer(navigationView);
             }
         });
 
@@ -172,12 +217,64 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
+    //Navigating between Menu Items
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @SuppressWarnings("StatementWithEmptyBody")
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem item) {
+                        // Create a new fragment and specify the fragment to show based on nav item clicked
+                        Fragment fragment = null;
+                        Class fragmentClass;
+                        // Handle navigation view item clicks here.
+                        int id = item.getItemId();
+                        switch(id) {
+                            case R.id.achievements:
+//                                fragmentClass = AchievementFragment.class;
+                                break;
+                            case R.id.my_findings:
+//                                fragmentClass = UserSubmissionFragment.class;
+                                break;
+                            case R.id.leaderboard:
+                                fragmentClass = LeaderboardFragment.class;
+                                break;
+//                            case R.id.notification:
+//                                fragmentSwitcher();
+//                                break;
+                            default:
+                                fragmentClass = MapFragment.class;
+                        }
+                        try {
+//                            fragment = (Fragment) fragmentClass.newInstance();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        // Insert the fragment by replacing any existing fragment
+                        FragmentManager fragmentManager = getFragmentManager();
+                        fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit();
+                        // Highlight the selected item has been done by NavigationView
+                        item.setChecked(true);
+
+                        // Close the navigation drawer after select item
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        return true;
+                    }
+                });
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
@@ -187,28 +284,5 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
 
-        if (id == R.id.achievements) {
-            // Handle the action like open achievement fragment
-        } else if (id == R.id.my_findings) {
-
-        } else if (id == R.id.leaderboard) {
-
-        } else if (id == R.id.notification) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
 }
