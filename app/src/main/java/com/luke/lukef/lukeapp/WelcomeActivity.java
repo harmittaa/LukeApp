2b07faafea7403f272cf0a8f6c954651a3e1bfe2
Daniel <daniel.zakharin>
2016-12-13 20:03:25 +0200

    link popup finished, fonts needs styling

--- app/src/main/java/com/luke/lukef/lukeapp/WelcomeActivity.java
+++ app/src/main/java/com/luke/lukef/lukeapp/WelcomeActivity.java
@@ -21,6 +21,7 @@ import com.auth0.android.lock.utils.LockException;
 import com.auth0.android.result.Credentials;
 import com.luke.lukef.lukeapp.model.Link;
 import com.luke.lukef.lukeapp.model.SessionSingleton;
+import com.luke.lukef.lukeapp.popups.LinkPopup;
 import com.luke.lukef.lukeapp.tools.LukeNetUtils;
 import com.luke.lukef.lukeapp.tools.LukeUtils;
 import com.luke.lukef.lukeapp.tools.SubmissionFetchService;
@@ -63,6 +64,8 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
         requestPermission();
         startService(new Intent(this, SubmissionFetchService.class));
         getCategories();
+        ShowLinkTask showLinkTask = new ShowLinkTask();
+        showLinkTask.execute();
     }
 
     private void requestPermission() {
@@ -91,9 +94,8 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
         }
     }
 
-    /*
-    AUTH0 STUFF
-     */
+
+
     private void doLogin(String clientId, String domain) {
         Auth0 auth0 = new Auth0(clientId, domain);
         this.lock = Lock.newBuilder(auth0, this.callBack).build(this);
@@ -216,19 +218,25 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
 
     private class ShowLinkTask extends AsyncTask<Void,Void,Void>{
 
+        Link link;
         @Override
         protected Void doInBackground(Void... params) {
-            Link jeeben = lukeNetUtils.getNewestLink();
+            link = lukeNetUtils.getNewestLink();
             return null;
         }
 
         @Override
         protected void onPostExecute(Void aVoid) {
+            if(link != null){
+                if(link.isActive()){
+                    creaeLinkPopup(link);
+                }
+            }
             super.onPostExecute(aVoid);
         }
     }
 
     private void creaeLinkPopup(Link l){
-
+        LinkPopup linkPopup = new LinkPopup(this,l);
     }
 }
